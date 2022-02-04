9261664ffafecacb0d7388035dac99b0e3d6bc2a
NicoleAlonso <80469471+NicoleAlonso@users.noreply.github.com>
2022-02-04 06:36:26 +0000

    finished assignment 1e

--- main.cpp
+++ main.cpp
@@ -200,7 +200,41 @@ Thing 1) Coffee Machine
     2) drips water over coffee grounds
     3) keeps coffee warm
  */
+struct CoffeeMachine
+{
+    // brand name
+    std::string brandName {"Krups"};
+    // amount of water the reservoir can contain 
+    float waterReservoirCapacity {2839.06f};
+    // number of water tubes 
+    int waterTubes {2};
+    // number of holes in drip area 
+    int numDripHoles {12};
+    // power of heating element 
+    float wattsOfHeatingElement {1200.0f};
 
+    struct Settings
+    {
+        int brewStrength {4};
+        int numCupsToBrew {2};
+        int minutesUntilShutOff {15};
+        float waterHardnessSetting {2.5f};
+        float waterFlowSpeed {0.8f};
+
+        void setTimer(float brewStart = 0, int minutesUntilShutOff = 20);
+        void setBrewStrength(int brewStrength);
+        bool flashDescalingIndicator(int waterHardness, int numCyclesMade); //returns true if the machine should run descaling program
+    };
+
+    // heat up water
+    void heatWater(float tempInCelsius = 93.4f);
+    // drip water over coffee grounds
+    void dripWater(Settings brewSettings, int dripInterval = 2);
+    // keep coffee warm
+    void maintainHeat(float timeToMaintain = 5.5f, float tempInCelsius = 85.0f);
+
+    Settings newBrewSettings;
+};
 /*
 Thing 2) Cargo Ship
 5 properties:
@@ -214,7 +248,41 @@ Thing 2) Cargo Ship
     2) load/unload goods
     3) burns fuel
  */
+struct CargoShip
+{
+    // size of deck 
+    float deckSize {12000.0f};
+    // size of fuel tank in million unit
+    float fuelTankSize {1.7f};
+    // amount of cargo 
+    int numOfCargoContainers {1875};
+    // ship name 
+    std::string shipName {"Neptuno"};
+    // captain name 
+    std::string captainName {"James Cook"};
 
+    struct CargoContent
+    {
+        std::string electronics {"home appliances"};
+        std::string clothing {"t-shirts"};
+        std::string food {"vegetables"};
+        std::string constructionMaterial {"wood"};
+        std::string furniture {"sofa"};
+
+        bool contentIsFlammable(std::string category, int igniteLevel = 1); //returns if the transported goods are flammable or not
+        bool contentIsToxic(std::string toxicityType, int classRating = 4); //returns if the transported goods are toxic or not
+        float numItemsPerContainer(int numItems, float singleItemSize, float containerSize = 38.5f); // returns how many goods fit into one container
+    };
+
+    // transport goods
+    void transportGoods(CargoContent ikeaItems, int amountOfContainers);
+    // load/unload goods
+    void handleGoods(CargoContent fridges, int amountOfContainers, bool shipIsEmpty = true);
+    // burn fuel
+    void burnFuel(float consumptionPerKm, float travelDistance, bool shipIsLoaded = true);
+
+    CargoContent nextCargoLoad;
+};
 /*
 Thing 3) Computer
 5 properties:
@@ -228,7 +296,23 @@ Thing 3) Computer
     2) process data
     3) display data
  */
+struct Computer
+{
+    // amount of storage 
+    double storageInGb {500.0};
+    // processor speed 
+    float processorSpeed {3.5f};
+    // display-size 
+    float displaySize {13.0f};
+    // amount of keys 
+    int numKeys {101};
+    // name of manufacturer
+    std::string manufacturer {"Apple"};
 
+    void storeData(int numFiles, std::string fileName = "unknown");
+    void processData(float amountToProcess, float timeToProcess = 0.02f);
+    void displayData(std::string fileName = "unknown", bool fullscreen = false);
+};
 /*
 Thing 4) Hotel
 5 properties:
@@ -242,7 +326,26 @@ Thing 4) Hotel
     2) serves breakfast
     3) charges guest
  */
+struct Hotel
+{
+    // amount of single bedrooms 
+    int numSingleRooms {46};
+    // amount of double bedrooms 
+    int numDoubleRooms {62};
+    // number of roomkeys 
+    int numRoomkeys {108};
+    // amount of daily customers 
+    int numDailyGuests {77};
+    // name of receptionist
+    std::string receptionistName {"Gabriel"};
 
+    // provide lodging
+    void provideLodging(Computer registerGuest, int daysToStay = 2, bool isDoubleRoom = true);
+    // serve breakfast
+    void serveBreakfast(int numGuestToServe = 65, bool isVegan = false);
+    // charge guest
+    void chargeGuest(int amountOfDays, float pricePerDay, bool usedRoomService = false, float priceRoomService = 25.5f);
+};
 /* 
 Thing 5) Rendering Engine
 5 properties:
@@ -256,7 +359,26 @@ Thing 5) Rendering Engine
     2) render material texture
     3) update camera view position
  */
+struct RenderingEngine
+{
+    // Lighting Type 
+    std::string lightingType {"spot light"};
+    // Amount of Particles (double)
+    double particleAmount {2300.0};
+    // Material Texture 
+    std::string materialTexture {"grass"};
+    // Amount of Bloom effect 
+    float bloomAmountInPercentage {10.0f};
+    // Degrees of Camera View Field 
+    float cameraViewField {180.0f};
 
+    //render lighting
+    void renderLights(std::string lightingType, int renderQuality = 4);
+    // render material texture
+    void renderMaterialTexture(std::string materialTexture = "stone", int renderQuality = 4);
+    // update camera view position
+    void updateCameraViewPosition(float cameraViewField, float positionX = 0.0f, float positionY = 0.0f);
+};
 /*
 Thing 6) Physics Engine
 5 properties:
@@ -270,7 +392,26 @@ Thing 6) Physics Engine
     2) set gravity
     3) destroy object
  */
+struct PhysicsEngine
+{
+    // Physics Type 
+    std::string physicsType {"default"};
+    // Collision Response Type 
+    std::string collisionResponse {"block"};
+    // Amount of Gravity 
+    float gravity {1.0f};
+    // Amount of Surface Friction 
+    float friction {4.0f};
+    // Raytracing Distance 
+    float raytracingDistance {100.0f};
 
+    //detect collision
+    void detectCollision(float raytracingDistance);
+    // set gravity
+    void setGravity(float gravity);
+    // destroy object
+    void destroyObject(std::string collisionResponse = "hit", std::string physicsType = "simulated", int impactForce = 9);
+};
 /*
 Thing 7) Audio Engine
 5 properties:
@@ -284,7 +425,26 @@ Thing 7) Audio Engine
     2) edit soundfile
     3) playback sound
  */
+struct AudioEngine
+{
+    // Number of I/O Channels 
+    int numChannels {2};
+    // Volume 
+    float volumeInDb {-18.0f};
+    // Pitch Multiplier 
+    float pitchMultiplier {1.0f};
+    // Attenuation 
+    float attenuationInDb {3.0f};
+    // Audio Buffer Size 
+    double audioBufferSize {256.0};
 
+    // import soundfile
+    void importSoundfile(std::string filePath, std::string fileName, float fileSizeInKb = 894.4f);
+    // edit soundfile
+    void editSound(float pitchMultiplier, float attenuationInDb = 6.0f);
+    // playback sound
+    void playSound(double audioBufferSize, int numChannels, float volumeInDb = -3.0f, bool loopSound = false);
+};
 /*
 Thing 8) Networking System
 5 properties:
@@ -298,7 +458,26 @@ Thing 8) Networking System
     2) share game state info
     3) send data to server
  */
+struct NetworkingSystem
+{
+    // Network Mode 
+    std::string networkMode {"standalone"};
+    // Priority Status 
+    int priorityStatus {1};
+    // Read Buffer Size 
+    double readBufferSize {1024};
+    // Send Rate 
+    float sendRate {60.0f};
+    // Protocol Type 
+    std::string protocolType {"UDP"};
 
+    // enable multiplayer mode
+    void setMultiplayerMode(std::string networkMode = "server", int numOfPlayers = 2);
+    // share game state info
+    void shareGameState();
+    // send data to server
+    void sendDataToServer(float sendRate, float dataSize);
+};
 /*
 Thing 9) AI
 5 properties:
@@ -312,7 +491,26 @@ Thing 9) AI
     2) move to target
     3) set focal point
  */
+struct AI
+{
+    // Acceptance Radius 
+    float acceptanceRadius {90.0f};
+    // Focal Point 
+    float focalPoint {1.0f};
+    // Movement Velocity 
+    int moveVelocity {5};
+    // Perception Properties 
+    std::string perceptionProperty {"hearing sense"};
+    // Stimuli Age (time until perceived sense is forgotten) 
+    float stimuliAgeInSec {5.0f};
 
+    // get target location
+    float getTargetLocation(float positionX = 10.0f, float positionY = 10.0f); //returns current target position 
+    // move to target
+    void moveToTarget(int moveVelocity, float acceptanceRadius = 50.5f);
+    // set focal point
+    void setFocalPoint(float focalPoint, float oldPosition, float newPosition);
+};
 /*
 Thing 10) Game Engine
 5 properties:
@@ -326,7 +524,26 @@ Thing 10) Game Engine
     2) implement audio
     3) get user input
  */
+struct GameEngine
+{
+    // Rendering Engine
+    RenderingEngine renderingEngine;
+    // Physics Engine
+    PhysicsEngine physicsEngine;
+    // Audio Engine
+    AudioEngine audioEngine;
+    // Networking System
+    NetworkingSystem networking;
+    // AI
+    AI arificialIntelligence;
 
+    //edit graphics
+    void editGraphics(RenderingEngine renderingEngine);
+    // implement audio
+    void implementAudio(AudioEngine audioEngine, std::string triggerEventName = "footsteps");
+    // get user input
+    void getUserInput(NetworkingSystem networkSettings, std::string controllerType = "PS4");
+};
 
 
 
